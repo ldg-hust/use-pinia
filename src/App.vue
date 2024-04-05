db3218d3ab3a304a0a949505fb885f3b468dc13c
ldg <3305473927.@qq.com>
2024-04-05 15:00:38 +0800

    pinia的简单用法

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
+
 <script setup>
 import { useCounterStore } from '@/stores/counter'
+import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
 
 const counterStore = useCounterStore();
 console.log(counterStore)
+//直接解析复制（对象数据响应式丢失）（方法函数可以）
+const {getList}=counterStore
+//storeToRefs包裹即可
+const {count,doubleCount}=storeToRefs(counterStore)
 
+onMounted(()=>{
+      getList();
+})
+console.log(counterStore.list)
 </script>
 
 <template>
-  <div>{{ counterStore.count }}</div>
+  <div>{{count }}</div>
+  <div>  {{ doubleCount }} </div>
   <button @click="counterStore.increment">加一</button>
+   <ul>
+    <li v-for="item in counterStore.list" :key="item.id">{{ item.name }}</li>
+   </ul>
+
+ <!-- // <button @click="counterStore.doubleCount">加倍</button> -->
 </template>
 
 <style scoped></style>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,11 +1,26 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import axios from 'axios'
 
 export const useCounterStore = defineStore('counter', () => {
     //定义数据（state）
     const count = ref(0);
+    //定义修改数据的方法
     const increment = () => {
         count.value++
     }
-    return { count, increment }
+
+    //getter定义
+    const doubleCount = computed(() => count.value * 2);
+
+    //异步action
+
+    const API_URL = 'http://geek.itheima.net/v1_0/channels'
+    const list = ref([]);
+    const getList = async () => {
+        const res = await axios.get(API_URL)
+        list.value = res.data.data.channels;
+    }
+    console.log(list)
+    return { count, increment, doubleCount, getList, list }
 })
